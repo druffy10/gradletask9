e412fe35ef4b83104db11d69a2991fefed0e8de3
d1sam <d1sam.dot@gmail.com>
2023-07-13 13:06:30 +0300

    fix commit

--- src/test/java/test/MoneyTransferTest.java
+++ src/test/java/test/MoneyTransferTest.java
@@ -49,10 +49,10 @@ public class MoneyTransferTest {
         var transferAmount = DataHelper.generateInvalidAmount(firstCardBalance);
         var transferPage = dashboardPage.selectCard(secondCardInfo);
         transferPage.makeTransfer(String.valueOf(transferAmount), firstCardInfo);
-        /*transferPage.errorMessage("Ошибка!");
+        transferPage.errorMessage("Ошибка!");
 
         Assertions.assertEquals(firstCardBalance, dashboardPage.getCardBalance(0));
-        Assertions.assertEquals(secondCardBalance, dashboardPage.getCardBalance(1));*/
+        Assertions.assertEquals(secondCardBalance, dashboardPage.getCardBalance(1));
     }
 }
 
